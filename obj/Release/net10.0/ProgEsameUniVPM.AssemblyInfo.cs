34952177bd1599e28ef817ccd9bc65823a1339ce
Saverio Di Francesco <savi.difra@gmail.com>
2026-04-30 11:36:54 +0200

    ho aggiunto pippo

--- obj/Release/net10.0/ProgEsameUniVPM.AssemblyInfo.cs
+++ obj/Release/net10.0/ProgEsameUniVPM.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgEsameUniVPM")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03f49947e19d4ab59d2d20ae53ee8a8bbf2d716")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgEsameUniVPM")]
